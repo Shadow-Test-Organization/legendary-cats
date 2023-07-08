6ab6e58110bcdd677c18fff605fa8f345f94edba
Ujjwal <138803882+shadowshot-x-1@users.noreply.github.com>
2023-07-08 13:49:41 +0530

    Update cats.py

--- cats.py
+++ cats.py
@@ -1,6 +1,6 @@
 # Legendary Cat Names
 
-print("American Shorthair") # American
+print("American Shorthair") # American Shorthair
 print("Persian Cat") # Persian
 print("Siamese Cat")
 print("Sphynx Cat")
