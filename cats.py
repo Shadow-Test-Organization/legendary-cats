607e66b7b92563fca210f2d90a684005c16c4892
Ujjwal <138803882+shadowshot-x-1@users.noreply.github.com>
2023-07-11 10:15:55 +0530

    Update cats.py

--- cats.py
+++ cats.py
@@ -6,8 +6,8 @@
 # Sphynx Cat
 # Ragdoll Cat
 
-print("American Shorthair") 
+print("American Shorthair Cat") 
 print("Persian Cat")
 print("Siamese Cat")
 print("Sphynx Cat")
-print("Ragdoll")
+print("Ragdoll Cat")
